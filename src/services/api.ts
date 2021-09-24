985d14c318629f6bdd1e6ce7cea672e52c66b8d5
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-09-24 16:07:30 -0300

    feat: add service api

---
+++ src/services/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+type QuoteAtomResponse = {
+  anime: number
+  character: string
+  quote: number
+}
+
+const api = axios.create({
+  baseURL: 'https://animechan.vercel.app/api/'
+})
+
+export const getQuote = async () => {
+  const { data } = await api.get<QuoteAtomResponse>('random')
+
+  return data
+}
